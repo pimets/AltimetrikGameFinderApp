ba2e414ff82e22516ca5810d53b3141941159d82
Xime Echavarria <pimets@gmail.com>
2022-06-18 03:54:52 -0300

    login with json server implemented

---
+++ Login/main.js
@@ -0,0 +1,63 @@
+/* 
+login.addEventListener('submit', buttonLogin);  
+
+function buttonLogin(e) {
+   e.preventDefault();
+   console.log('lpm');
+} */
+
+//Getting data from the post, see if is neccesary
+fetch("http://localhost:3000/users")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+// POST
+
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var submit = document.getElementById("login");
+
+submit.addEventListener("click", formSubmit);
+
+function formSubmit(e) {
+    e.preventDefault();
+    // validation email && validation password
+  if (validateEmail(email.value) && validatePassword(password.value)) {
+    login(email.value, password.value);
+  }
+}
+
+
+function login(mail, password) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    email: mail,
+    password: password,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:3000/login", requestOptions)
+    .then((response) => response.text())
+    .then((result) => window.location.href = '../GameFinder/gameFinder.html') 
+    .catch((error) => popUpSnackbar()); //here goes the snackbar
+
+
+/* function popUpSnackbar(){
+    // Get the snackbar DIV
+    const x = document.getElementById("snackbar");
+    // Add the "show" class to DIV
+    x.className = "show";
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ 
+        x.className = x.className.replace("show", ""); 
+    }, 
+    3000);
+    } */
--- Login/validation.js
+++ Login/validation.js
@@ -1,15 +1,30 @@
-const emailEl = document.querySelector('#email');
+/* const emailEl = document.querySelector('#email');
 const passwordEl = document.querySelector('#password');
 
-const login = document.querySelector('.form1');
+const login = document.querySelector('.form1'); */
 
 
 /* const form = document.querySelector('#login'); */
 
-login.addEventListener('submit', buttonLogin);  
+  
 
- function buttonLogin(evt) {
-    evt.preventDefault();
-    console.log('lpm');
-} 
+function validateEmail(mail) {
+    //Regular expression
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      return true;
+    }
+    return false;
+  }
+  
+  function validatePassword(password) {
+    //Regular expression
+    if (
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,14}$/.test(
+        password
+      )
+    ) {
+      return true;
+    }
+    return false;
+  }
 
